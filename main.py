8adb796f278366eefb309923532c07beb839a4d4
Maria Eduarda <duda.lira2017@outlook.com>
2026-06-05 19:02:39 -0300

    feat: adiciona seleção de matérias

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-import flet as ft
+import flet as ft   # Importei a classe flet e dei um apelido de ft.
 
+# função principal do aplicativo
 def main(page: ft.Page):
 
-    # "page" janela do aplicativo.
-    # estamo definindo o título da janela.
+    # "page" representa a janela do aplicativo.
+    # aqui definimos o título da janela.
     page.title = "Horas Líquidas"
 
-    # variável que guarda um componente visual.
+    # variável que guarda um componente visual de texto.
     titulo = ft.Text(
+        # conteúdo exibido na tela.
         value="Cronômetro de Estudos",
+
+        # tamanho da fonte.
         size=30
     )
 
-    # cria um botão visual.
+    materias = ft.Dropdown(
+        label="Selecione a matéria",
+        options=[
+            ft.dropdown.Option("Português"),
+            ft.dropdown.Option("T.I"),
+            ft.dropdown.Option("Matemática"),
+            ft.dropdown.Option("Conhecimentos Bancários")
+        ]
+    )
+
+    # função de evento.
+    # será executada quando o botão for clicado.
     def iniciar(e):
+
+        # altera dinamicamente o texto do componente.
         titulo.value = "Estudando..."
+
+        # atualiza/renderiza a interface gráfica.
         page.update()
 
+    # cria um componente visual de botão.
     botao = ft.Button(
+
+        # conteúdo interno do botão.
         content=ft.Text("Iniciar"),
+
+        # conecta o clique do botão à função iniciar.
         on_click=iniciar
     )
-  
-    # flet pega os componentes título e botão e renderiza na janela.
-    page.add(titulo, botao)
 
-# a função principal vai ser a "main"
+    # adiciona os componentes na janela do aplicativo.
+    page.add(titulo, materias, botao)
+
+# inicia a aplicação executando a função principal.
 ft.run(main)
